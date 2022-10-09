d4a95f6cb660c17d1fb21927f711305d5355c05c
TOGoS <togos00@gmail.com>
2022-10-09 15:18:19 -0500

    Fix missing PrefixLogger def

--- Logger.ts
+++ Logger.ts
@@ -35,6 +35,23 @@ export class LevelFilteringLogger implements Logger {
 	}
 }
 
+export class PrefixLogger implements Logger {
+	public constructor(protected prefix: string, protected parent:Logger) {}
+
+	// deno-lint-ignore no-explicit-any
+	public log(thing:any, ...stuff:any[]) {
+		this.parent.log(this.prefix, thing, ...stuff);	
+	}
+	// deno-lint-ignore no-explicit-any
+	public warn(thing:any, ...stuff:any[]) {
+		this.parent.warn(this.prefix, thing, ...stuff);
+	}
+	// deno-lint-ignore no-explicit-any
+	public error(thing:any, ...stuff:any[]) {
+		this.parent.error(this.prefix, thing, ...stuff);
+	}
+}
+
 /** A logger that ignores everything */
 export const NULL_LOGGER:Logger = {
 	error(_message?: unknown, ..._optionalParams: unknown[]): void { },
